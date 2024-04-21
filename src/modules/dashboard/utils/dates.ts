6f1e1aa5381a43ce1c670aa5f1b09a19f2e9b7e2
dumik <dumik.alive@gmail.com>
2024-04-21 18:54:44 +0300

    feat: remove console log

--- src/modules/dashboard/utils/dates.ts
+++ src/modules/dashboard/utils/dates.ts
@@ -2,7 +2,6 @@ import moment from 'moment';
 
 export const getDaysBetweenDates = (startDate?: moment.Moment, endDate?: moment.Moment): number => {
   if (endDate && endDate) {
-    console.log('%c jordan endDate', 'color: lime;', endDate, startDate);
     return moment(endDate).diff(moment(startDate), 'days') + 1;
   }
   return 0;
